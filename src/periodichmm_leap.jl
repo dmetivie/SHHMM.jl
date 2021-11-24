5b4ee5ca730deb2b28be1723c3251a68e149df08
David Métivier <46794064+dmetivie@users.noreply.github.com>
2021-11-24 16:03:24 +0100

    change likelihoods! for PeriodicHMM in correct fil

--- src/periodichmm_leap.jl
+++ src/periodichmm_leap.jl
@@ -124,6 +124,42 @@ day2(y, t) = (1 + y + 2t)
 
 ###
 
+function likelihoods!(L::AbstractMatrix, hmm::PeriodicHMM{Univariate}, observations, n2t::AbstractArray{Int})
+    N, K, T = size(observations, 1), size(hmm, 1), size(hmm, 3)
+    @argcheck size(LL) == (N, K)
+    @inbounds for i in OneTo(K), n in OneTo(T)
+        t = n2t[n] # periodic t
+        L[n, i] = pdf(hmm.B[i, t], observations[n])
+    end
+end
+
+function likelihoods!(L::AbstractMatrix, hmm::PeriodicHMM{Multivariate}, observations, n2t::AbstractArray{Int})
+    N, K, T = size(observations, 1), size(hmm, 1), size(hmm, 3)
+    @argcheck size(LL) == (N, K)
+    @inbounds for i in OneTo(K), n in OneTo(T)
+        t = n2t[n] # periodic t
+        L[n, i] = pdf(hmm.B[i, t], view(observations, n, :))
+    end
+end
+
+function loglikelihoods!(LL::AbstractMatrix, hmm::PeriodicHMM{Univariate}, observations, n2t::AbstractArray{Int})
+    N, K, T = size(observations, 1), size(hmm, 1), size(hmm, 3)
+    @argcheck size(LL) == (N, K)
+    @inbounds for i in OneTo(K), n in OneTo(N)
+        t = n2t[n] # periodic t
+        LL[n, i] = logpdf(hmm.B[i, t], observations[n])
+    end
+end
+
+function loglikelihoods!(LL::AbstractMatrix, hmm::PeriodicHMM{Multivariate}, observations, n2t::AbstractArray{Int})
+    N, K, T = size(observations, 1), size(hmm, 1), size(hmm, 3)
+    @argcheck size(LL) == (N, K)
+    @inbounds for i in OneTo(K), n in OneTo(N)
+        t = n2t[n] # periodic t
+        LL[n, i] = logpdf(hmm.B[i, t], view(observations, n, :))
+    end
+end
+
 ####
 
 function update_A!(
